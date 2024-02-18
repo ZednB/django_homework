a9ce4521ac7ca56676020c4a677cb42851eccda7
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-02-18 22:04:48 +0300

    Применил миграции, добавиль новое поле в модель продукта и снова применил миграцию

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена')
     created_date = models.DateTimeField(verbose_name='Дата создания')
     uploaded_at = models.DateTimeField(verbose_name='Дата изменения')
+    manufactured_at = models.DateTimeField(default=True, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f"{self.name}({self.description}). Цена {self.price}"
--- config/settings.py
+++ config/settings.py
@@ -78,6 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_homework',
+        'USER': 'postgres',
+        'PASSWORD': '1111'
     }
 }
 
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
