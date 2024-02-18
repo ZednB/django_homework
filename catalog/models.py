06be2edc109fe0a1163f92b3a02923ecf09e0ea4
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-02-18 21:55:21 +0300

    Описал поведения моделей

--- catalog/models.py
+++ catalog/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
-
-class Product(models.Model):
-    pass
-
-    def __str__(self):
-        pass
-
-    class Meta:
-        pass
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
-        pass
+        return f"{self.name} ({self.description})"
 
     class Meta:
-        pass
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    photo = models.ImageField(upload_to='product/', **NULLABLE, verbose_name='Фото')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_date = models.DateTimeField(verbose_name='Дата создания')
+    uploaded_at = models.DateTimeField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f"{self.name}({self.description}). Цена {self.price}"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
