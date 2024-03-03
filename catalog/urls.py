750f3ef153f65461da5ec0bae99a006bcffadbbb
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-03-04 01:09:55 +0300

    Перевел контроллеры на CBV, создаль новую модель со всеми полями, написал два класса crud

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.views import ProductListView, ProductDetailView, ContactView, BlogCreateView, \
-    BlogListView
+    BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, toggle_activity
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
@@ -8,5 +8,9 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 
     path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
     path('list/', BlogListView.as_view(), name='list_blog'),
+    path('detail/<int:pk>/', BlogDetailView.as_view(), name='detail_blog'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog'),
+    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
 ]
