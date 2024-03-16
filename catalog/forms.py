2f9fe26904de742d126b1077590cdc94e3967ad6
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-03-16 17:50:52 +0300

    Два задания сделал

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,9 +3,15 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     def clean_name(self):
         cleaned_name = self.cleaned_data['name']
